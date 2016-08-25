02028283063ac039f83cae49bcff1293f5b835a1
chetan <chetan_madnaik@neovasolutions.com>
2016-08-25 15:17:15 +0530

    created chets.txt --just checking whether it is commiting. :)

---
+++ acb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  static int i=0,j=2;
+scanf("enter the string =",&i);
+while(i++)
+{
+printf("geometric progresin",j*j);
+
+if(i=10)
+break;
+}
+
+return;
+}
